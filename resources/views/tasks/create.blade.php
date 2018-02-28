54a8223be4ec323e7dbaaa6f68d624f20072afb4
kumaguardian <kuma.guardian2018@gmail.com>
2018-02-28 08:50:57 +0000

    bootstrap design edit

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,19 +7,8 @@
         {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
     <div class="row">
-        <div class="col-sm-8"></div>
-        <div class="col-sm-offset-2 col-sm-8"></div>
-        
-    <div class="row">
-         <div class="col-md-8"></div>
-         <div class="col-md-offset-2 col-md-8"></div>
-     </div>    
-        
-    <div class="row">
-         <div class="col-lg-6"></div>
-         <div class="col-lg-offset-3 col-lg-6"></div>
-    </div>     
-        
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
